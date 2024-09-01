8cc7cf863b2e5146d91adf3657a6fcc9f6cc076f
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2024-09-01 09:14:01 +0300

    Time: 7 ms (98.67%), Space: 21.9 MB (66.53%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int zp = 0,np = 0,sz = nums.size(); //zero pointer and number pointer
-        while(zp < sz && nums[zp] != 0)
-            zp++;
-        np = zp + 1;
-        while(np < sz){
-            if(nums[np] != 0){
-                while(zp < sz && nums[zp] != 0)
-                    zp++;
-                if(zp < np)
-                    swap(nums[np],nums[zp]);
+        int curInd = 0;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[i]!=0){
+                nums[curInd] = nums[i];
+                curInd++;
             }
-            np++;
         }
+        for(int i = curInd ; i < nums.size() ; i++)
+           nums[i] = 0;
+            
     }
 };
