e260b256f4375a18dbecd8313f6e125315a622c9
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2024-09-19 23:46:44 +0300

    Time: 7 ms (18.8%), Space: 10.8 MB (73.89%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.cpp
@@ -1,6 +1,34 @@
 class Solution {
 public:
-    vector<int> diffWaysToCompute(string expression) {
-        
-    }
+int operation(int a,int b,char op)
+{
+  if(op=='*')
+  return a*b;
+  if(op=='/')
+  return a/b;
+  if(op=='+')
+  return a+b;
+  if(op=='-')
+  return a-b;
+  return 0;
+}
+    vector<int> diffWaysToCompute(string ex)
+{
+   vector<int>allres;
+   for( int i=0;i<ex.size();i++)
+     {
+      char c=ex[i];
+      if(c=='+'||c=='-'||c=='*')
+      {
+        vector<int>resleft=diffWaysToCompute(ex.substr(0,i));//send left string 
+        vector<int>resright=diffWaysToCompute(ex.substr(i+1));//send right string 
+         for(auto x :resleft )
+            for(auto y : resright)
+             allres.push_back(operation(x,y,c));
+      }
+     } 
+      if(allres.size()==0)
+        allres.push_back(stoi(ex));
+        return allres;
+}
 };
