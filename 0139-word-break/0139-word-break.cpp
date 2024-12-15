c4d9fec86579904516d4f2a93112e8e2c3304a02
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2024-12-15 20:45:39 +0200

    Time: 0 ms (100%), Space: 10.3 MB (90.6%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string, bool> vis;
-        for (auto word : wordDict) {
-            vis[word] = true;
-        }
 
         vector<bool> dp(s.size() + 1, false); 
         dp[0] = true;
 
         for (int i = 1; i <= s.size(); i++) {
-            for (int j = 0; j < i; j++) {
-                if (dp[j]) {
-                    string word = s.substr(j, i - j); 
-                    if (vis.find(word) != vis.end()) { 
-                        dp[i] = true;
-                        break;
-                    }
+            for(auto &x : wordDict) {
+                int j = i - x.size() ;
+                if (j >= 0 && dp[j]&& s.substr(j,x.size()) == x) {
+                    dp[i] = true;
+                    break;
                 }
+                
             }
         }
 
