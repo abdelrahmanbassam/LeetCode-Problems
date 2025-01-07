d43bbe292c280ac5c99cb1e75693e5054d64cf85
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-01-07 02:21:57 +0200

    Time: 2 ms (53.69%), Space: 11.2 MB (76.01%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.cpp
@@ -5,7 +5,7 @@ public:
         vector<string> res;
 		for(int i = 0; i < words.size(); i++)
 			for(int j = 0; j < words.size(); j++)
-				if(words[i] != words[j] and words[j].find(words[i]) != -1){
+				if(words[i] != words[j] && words[j].find(words[i]) != -1){
 					res.push_back(words[i]);
 					break;
                 }                    
