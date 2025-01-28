8223ec0147ea115abdadfe2da6502eab4ed22a04
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-01-28 14:01:16 +0200

    Time: 2 ms (61.51%), Space: 19.8 MB (16.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -4,16 +4,12 @@ public:
         unordered_map<int, int> freqs;
         for(auto &e : nums)
             freqs[e]++;
-
         vector<vector<int>>buckets(nums.size());
         for(auto &e : freqs){
             buckets[e.second-1].push_back(e.first);
         }
         vector<int> ans;
-        for(int i = nums.size() - 1; i >= 0 && k; i-- ){
-            if(!buckets[i].size())
-                continue;
-
+        for(int i = nums.size() - 1; i >= 0; i-- ){
             for(auto &e : buckets[i]){
                 if(!k)
                     break;
