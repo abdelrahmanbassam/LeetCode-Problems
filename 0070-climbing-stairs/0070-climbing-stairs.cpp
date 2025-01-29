eda39877cc90c567dd9857c8ea11a95063f85f3b
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-01-29 22:19:52 +0200

    Time: 0 ms (100%), Space: 7.8 MB (87.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,9 +3,9 @@ public:
     int climbStairs(int n) {
         if(n <= 2)
             return n;
-        int f = 1, s = 2;
+        int f = 1, s = 2,temp;
         for(int i = 3; i <= n; i++){
-            int temp = s;
+            temp = s;
             s += f;
             f = temp;
         } 
