2312d8a904b076fb2b3f7764b89c7a51ca4c3c84
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-02-15 17:18:48 +0200

    Time: 0 ms (100%), Space: 15.1 MB (66.24%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -6,19 +6,11 @@ public:
             int mid = (l+r)/2;
             if(nums[mid] == target)
                 return mid;
-            if(nums[mid] >= nums[l])
-            {
-                if(nums[mid] > target && target >= nums[l])
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-            else{
-                if(nums[mid] < target && target <= nums[r])
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
+            if(nums[mid] >= nums[l] && !(target >= nums[l] && target < nums[mid])
+            || target >= nums[mid] && target <= nums[r] )
+                l = mid + 1;
+            else
+                 r = mid - 1;
         }
         return -1;
     }
