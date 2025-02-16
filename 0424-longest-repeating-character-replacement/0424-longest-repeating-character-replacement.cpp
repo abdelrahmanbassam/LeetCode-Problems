e7de6352767341340788dec43af29d89aa073f12
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-02-17 00:20:52 +0200

    Time: 9 ms (39.81%), Space: 11.1 MB (21.25%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        unordered_map<char,int>mp;
-        int l = 0,ans=0,mxOc = 0 ;
+        unordered_map<char,int> freq;
+        int maxFreq = 0, ans = 0 , l = 0 ;
         for(int i = 0; i < s.size(); i++){
-            mp[s[i]]++;
-            mxOc = max(mxOc,mp[s[i]]);
-            while(i-l+1 - mxOc > k )
-                mp[s[l++]]--;
-            ans = max(ans, i - l +1);
-        }    
-        return ans;  
+            freq[s[i]]++;
+            maxFreq = max(maxFreq,freq[s[i]]);
+            while(i - l + 1 - maxFreq > k){
+                freq[s[l++]]--;
+            }
+            ans = max(ans,i - l +1);
+        }
+        return ans;
     }
 };
