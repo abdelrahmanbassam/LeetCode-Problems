71a1195c66d03f3ab1a084d0056b11c7827d9efe
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-02-18 01:09:15 +0200

    Time: 3 ms (20.68%), Space: 71.6 MB (80.66%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    int maxSubArrayHelper(int l, int r, vector<int>& nums){
-        if(l == r)
-            return nums[l];
-        int sumleft = maxSubArrayHelper(l, (l+r)/2, nums);
-        int sumRight = maxSubArrayHelper((l+r)/2 +1 , r, nums);
-        int sumCross = corssSum(l,r,nums);
-        return max(max(sumleft,sumRight),sumCross);
-    }
-    int corssSum(int l, int r, vector<int>& nums){
-        int sum = 0;
-        int leftSum = 0, rightSum = 0, mid = (l+r)/2;
-        for(int i = mid - 1 ; i >= l ; i--){
-            sum += nums[i];
-            leftSum = max(leftSum,sum);
-        }
-        sum = 0;
-        for(int i = mid + 1; i <= r; i++){
-            sum += nums[i];
-            rightSum = max(rightSum,sum);
-        }
-        return rightSum + leftSum + nums[mid];
-    }
     int maxSubArray(vector<int>& nums) {
-        return maxSubArrayHelper(0,nums.size()-1,nums);
+        int ans = nums[0], curSum = 0;
+        for(int i = 0 ; i < nums.size(); i++){
+            curSum += nums[i];
+            ans = max(ans,curSum);
+            if(curSum < 0 )
+                curSum = 0;
+        }
+        return ans;
     }
 };
