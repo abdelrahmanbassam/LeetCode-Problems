9d40cf2533ea7bace2dac9ac1cb7e52184e31768
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-02-18 01:33:02 +0200

    Time: 0 ms (100%), Space: 52.3 MB (30.82%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        bool canJ = true;
-        int zeroIndx = 0;
-        for(int i = nums.size() - 2; i >= 0 ; i--){
-            if(nums[i] == 0){
-                zeroIndx = max(zeroIndx,i);
-                canJ = false;
-            }
-            else{
-                if(nums[i] + i > zeroIndx){
-                    canJ = true;
-                    zeroIndx = 0;
-                }
+        int n = nums.size() ;
+        int reachable = n - 1;
+        for(int i = n - 2; i >= 0 ; i--){
+            if(nums[i] + i >= reachable){
+               reachable = i ;
             }
         }
-        return canJ;
+        return reachable == 0 ;
     }
 };
