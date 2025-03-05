2df5f98765fe9e747633a8ddbb74f3333343a9bd
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-03-05 17:30:09 +0200

    Time: 0 ms (100%), Space: 8 MB (31.17%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.cpp
@@ -1,14 +1,6 @@
 class Solution {
 public:
     long long coloredCells(int n) {
-        long long numCells = 0;
-        int iter = n - 1;
-        int add = 1;
-        while(iter--){
-            numCells += 2*add;
-            add+=2;
-        }
-        numCells += add;
-        return numCells;
+        return 1+ (4) * ((long long)n*(n-1)/2);
     }
 };
