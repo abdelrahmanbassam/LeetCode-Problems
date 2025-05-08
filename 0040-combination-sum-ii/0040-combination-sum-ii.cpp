675bcb56503b3eaa12a75938ec7fbf73f106bca4
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-05-09 00:14:22 +0300

    Time: 0 ms (100%), Space: 14.1 MB (38.92%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
+    vector<vector<int>> allValidComps;
+    void generateUniqueComps(vector<int>& candidates,vector<int>& curComp,int target, int i){
+        if(i >= candidates.size() || target < 0 ){
+            if(target == 0)
+                allValidComps.push_back(curComp);
+            return;
+        }
+        // if(target - candidates[i] < 0)
+        //     return;
+        curComp.push_back(candidates[i]);
+        generateUniqueComps(candidates,curComp,target - candidates[i],i + 1);
 
-     void helper(vector<int>& num,int i,vector<vector<int>> &res ,vector<int>& sub,int subSum,int tarSum){
-       if( i == num.size() || subSum >= tarSum ){
-         if(subSum==tarSum)
-          res.push_back(sub);
-          
-         return ;
-       }
-
-       sub.push_back(num[i]);
-       helper(num,i+1,res,sub,subSum+num[i],tarSum);
-      
-       sub.pop_back();
-        while(i < num.size()-1 &&num[i]==num[i+1] )
-         i++;
-       helper(num,i+1,res,sub,subSum,tarSum);
-       
- 
+        curComp.pop_back();
+        while(i < candidates.size()-1 && candidates[i] == candidates[i+1])
+            i++;
+        generateUniqueComps(candidates,curComp,target,i+1);
     }
 
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> res;
-        vector<int> sub;
         sort(candidates.begin(),candidates.end());
-        helper(candidates,0,res,sub,0,target);
-        return res;
+        vector<int> comp;
+        generateUniqueComps(candidates,comp,target,0);
+        return allValidComps;
+
     }
 };
