2fe0fe7286b2dbbe539249a6cdf823b685a8b286
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-05-08 23:58:36 +0300

    Time: 7 ms (32.14%), Space: 14.3 MB (43.28%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
-        sort(nums.begin(), nums.end());
-        dfs(0, {}, 0, nums, target);
-        return res;
-    }
-
-    void dfs(int i, vector<int> cur, int total, vector<int>& nums, int target) {
-        if (total == target) {
-            res.push_back(cur);
+    vector<vector<int>> validSubsets;
+    void getValidSubset(vector<int>& candidates,vector<int>& curComp, int &target,int i,int curSum){
+        if(i == candidates.size() || target <= curSum){
+            cout<<curSum<<" ";
+            if(target == curSum)
+                validSubsets.push_back(curComp);
             return;
         }
-        
-        for (int j = i; j < nums.size(); j++) {
-            if (total + nums[j] > target) {
-                return;
-            }
-            cur.push_back(nums[j]);
-            dfs(j, cur, total + nums[j], nums, target);
-            cur.pop_back();
-        }
+        if(curSum + candidates[i] > target)
+            return;
+        curComp.push_back(candidates[i]);
+        curSum += candidates[i];
+        getValidSubset(candidates,curComp,target,i,curSum);
+
+        curComp.pop_back();
+        curSum -= candidates[i];
+        getValidSubset(candidates,curComp,target,i+1,curSum);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> curComp;
+        sort(candidates.begin(),candidates.end());
+        getValidSubset(candidates,curComp,target,0,0);
+        return validSubsets;
     }
 };
