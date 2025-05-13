60a5ac0d2c06fb0375d0e5dc15dc8145044edd51
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-05-13 08:07:35 +0300

    Time: 0 ms (100%), Space: 9.7 MB (40.08%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,28 +1,23 @@
 class Solution {
-    vector<vector<int>> dp;
-
 public:
     bool isInterleave(string s1, string s2, string s3) {
         int m = s1.length(), n = s2.length();
         if (m + n != s3.length()) 
             return false;
-        dp = vector<vector<int>>(m + 1, vector<int>(n + 1, -1));
-        return dfs(0, 0, 0, s1, s2, s3);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        return dfs(0, 0, 0, s1, s2, s3, dp);
     }
-    
-    bool dfs(int i, int j, int k, string& s1, string& s2, string& s3) {
-        if(k == s3.size()){
-            return i == s1.size() && j == s2.size();
-        }
-        if(dp[i][j] != -1){
+    bool dfs(int i,int j,int x,string &s1,string &s2,string &s3,vector<vector<int>> &dp){
+        if(x == s3.size())
+            return true;
+        if(dp[i][j] != -1)
             return dp[i][j];
-        }
         bool found = false;
-        if(i < s1.size() && s1[i] == s3[k]){
-            found = dfs(i+1,j,k+1,s1,s2,s3);
+        if(i < s1.size() && s1[i] == s3[x]){
+            found = dfs(i+1,j,x+1,s1,s2,s3,dp);
         }
-        if(!found && j < s2.size() && s2[j] == s3[k]){
-            found = dfs(i,j+1,k+1,s1,s2,s3);
+        if(!found && j < s2.size() && s2[j] == s3[x]){
+            found = dfs(i,j+1,x+1,s1,s2,s3,dp);
         }
         return dp[i][j] = found;
     }
