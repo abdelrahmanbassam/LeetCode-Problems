b92ea07e7ddff31d37f87e187337f28bcc8a523a
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-05-13 07:50:09 +0300

    Time: 7 ms (71.79%), Space: 26.4 MB (25.22%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
-    int totalSum;
-
-    int findTargetSumWays(vector<int>& nums, int target) {
-       totalSum = accumulate(nums.begin(), nums.end(), 0);
-
-        vector<vector<int>>dp(nums.size() + 1, vector<int>(totalSum * 2 + 1,-1));
-        return memoBacktrack(0, 0, nums, target,dp);
-    }
-    int memoBacktrack(int i , int curSum, vector<int>& nums, int target, vector<vector<int>>&dp){
-        if(i == nums.size())
+    int topDown (vector<vector<int>> &dp,int i,vector<int>& nums, int target, int curSum,int totalSum){
+        if(i == nums.size()){
             return curSum == target;
-        if(dp[i][curSum + totalSum] != -1)
+        }
+        if(dp[i][curSum + totalSum] != -1){
             return dp[i][curSum + totalSum];
-        return dp[i][curSum + totalSum] =memoBacktrack(i+1,curSum + nums[i],nums,target,dp) +
-                                memoBacktrack(i+1,curSum - nums[i],nums,target,dp);
+        }
+        return dp[i][curSum+ totalSum] = topDown(dp,i+1,nums,target,curSum + nums[i],totalSum) + topDown(dp,i+1,nums,target,curSum - nums[i],totalSum);
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+
+        vector<vector<int>> dp(nums.size() + 1, vector<int>(totalSum * 2 + 1,-1));
+        topDown(dp,0,nums,target,0,totalSum);
+        return dp[0][totalSum];
+        // for(int i = 0; i <= n; i++){
+        //     dp[i][0] = 1;
+        // }
+        // for(int i = 1; i <= n; i++){
+        //     for(int a = 1; a <= target; a++){
+        //         dp[i][a] += dp[i]
+        //     }
+        // }
+        // return dp[n][target];
     }
 };
