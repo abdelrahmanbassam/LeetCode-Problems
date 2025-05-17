97d80be3fe3f1843771555a6baa5c46bc128374c
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-05-17 18:21:21 +0300

    Time: 11 ms (31.44%), Space: 13.1 MB (61.42%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,31 @@
 class Solution {
 public:
+    int minOpeartions(string &s, string &t,int i, int j , vector<vector<int>> &dp){
+        if(i == s.size()){
+            return t.size() - j;
+        }
+        if(j == t.size()){
+            return s.size() - i;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int insertCost,deleteCost,replaceCost;
+        if(s[i] == t[j]){
+            return dp[i][j] = minOpeartions(s,t,i+1,j+1,dp);
+        }
+        else{
+            insertCost = 1+ minOpeartions(s,t,i,j+1,dp);
+            deleteCost = 1+ minOpeartions(s,t,i+1,j,dp);
+            replaceCost = 1+ minOpeartions(s,t,i+1,j+1,dp);
+        }
+        return dp[i][j] = min(insertCost,min(deleteCost,replaceCost));
+    }
     int minDistance(string word1, string word2) {
-        int n = word1.length();
-        int m = word2.length();
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        int n =word1.size(), m = word2.size();
+        vector<vector<int>>dp(n,(vector<int>(m,-1)));
         
-        for (int i = 1; i <= n; ++i)
-            dp[i][0] = i;
-
-        for (int j = 1; j <= m; ++j)
-            dp[0][j] = j;
-
-        for (int i = 1; i <= n; ++i)
-            for (int j = 1; j <= m; ++j)
-            if (word1[i - 1] == word2[j - 1])//same characters
-                dp[i][j] = dp[i - 1][j - 1];//no operation
-            else
-                dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
-                             //replace       //delete        //insert
-    return dp[n][m];
-        
+        return minOpeartions(word1,word2,0,0,dp);
     }
 };
