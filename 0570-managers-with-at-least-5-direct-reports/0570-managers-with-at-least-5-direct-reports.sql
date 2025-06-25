ce129d7a9d773370363be4c53354127dd3dd49c7
Abdelrahman Bassam <119951925+abdelrahmanbassam@users.noreply.github.com>
2025-06-25 11:38:52 +0300

    Time: 329 ms (82.59%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,6 @@
 # Write your MySQL query statement below
-select m.name 
-from Employee m 
-inner join Employee e on m.id = e.managerId
+select m.name
+from employee m 
+join employee e on m.id = e.managerId 
 group by m.id
-having count(*) >= 5 
-
+having count(*) >= 5
